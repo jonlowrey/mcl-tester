015fa7a0b71de2be6ac78002319d841d5b287644
jonlowrey <jon.lowrey@tempus.com>
2021-06-25 14:26:59 -0500

    in progress

--- App.js
+++ App.js
@@ -2,11 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button, ButtonStyle } from '@mcl-boron-components/button'
+import { SpinnerBig, SpinnerSmall } from '@mcl-boron-components/spinner';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Tester for MCL</Text>
+      <SpinnerBig />
+      <SpinnerSmall />
       <Button displayText="Primary Button" onPress={() => {}} type={ButtonStyle.PRIMARY} /> 
       <Button displayText="Secondary Button" onPress={() => {}} type={ButtonStyle.SECONDARY} /> 
       <Button displayText="Tertiary Button" onPress={() => {}} type={ButtonStyle.TERTIARY} />
