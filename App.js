bfdcd7e4328c3475638907478bc1169c5e318b31
jonlowrey <jon.lowrey@tempus.com>
2021-06-30 10:48:30 -0500

    try building this with XCode 12.5 in Big Sur

--- App.js
+++ App.js
@@ -3,15 +3,13 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Button, ButtonStyle } from '@mcl-boron-components/button'
 import { SpinnerBig, SpinnerSmall } from '@mcl-boron-components/spinner';
-import { LottieView } from 'lottie-react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Tester for MCL</Text>
-      {/* <SpinnerBig />
-      <SpinnerSmall /> */}
-      <LottieView source={require('./assets/animations/LineAnimation.json')} autoPlay loop style={styles.small} />
+      <SpinnerBig />
+      <SpinnerSmall />
       <Button displayText="Primary Button" onPress={() => {}} type={ButtonStyle.PRIMARY} /> 
       <Button displayText="Secondary Button" onPress={() => {}} type={ButtonStyle.SECONDARY} /> 
       <Button displayText="Tertiary Button" onPress={() => {}} type={ButtonStyle.TERTIARY} />
@@ -26,5 +24,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  small: {
+    width: 40,
+    height: 40,
   },
 });
